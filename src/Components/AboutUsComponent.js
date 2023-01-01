1c8887bd34f80e13b38bea4d892e5cb9fa4d581d
spunuru <spunuru@landmarkhealth.org>
2023-01-01 19:55:02 +0530

    Adding outlet for AboutUs Component

--- src/Components/AboutUsComponent.js
+++ src/Components/AboutUsComponent.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import { Outlet } from "react-router-dom";
 import { title } from '../Common/Constants.js';
 
 const AboutUsComponent=()=>{
@@ -6,6 +7,7 @@ const AboutUsComponent=()=>{
         <React.Fragment>
             <h1>About Us</h1>
             <h2>Our team name is {title}</h2>
+            <Outlet/>
             <h3>Team Captain : Harshita Mangla</h3>
             <h4>Team Size : 10</h4>
             <h5>We all meet in Namaste React BootCamp</h5>
